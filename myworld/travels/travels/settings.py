d35236698dba9d09653951720f3145c5dd6769a6
sneha931 <153498006+sneha931@users.noreply.github.com>
2025-08-28 20:15:13 +0530

    Remove trailing slash from CORS allowed origin

--- myworld/travels/travels/settings.py
+++ myworld/travels/travels/settings.py
@@ -142,7 +142,7 @@ CORS_ALLOWED_ORIGINS = [
     "http://127.0.0.1:5173",
     "http://localhost:3000",
     "http://127.0.0.1:3000",
-    "https://travel-ticket-booking.vercel.app/",
+    "https://travel-ticket-booking.vercel.app",
 ]
 
 CORS_ALLOW_CREDENTIALS = True
